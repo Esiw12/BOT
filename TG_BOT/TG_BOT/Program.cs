af0b646d814e3dc9ebd3ae6a1e75b208b173f0a6
Esiw[Ярослав] <65117234+Esiw12@users.noreply.github.com>
2023-10-16 16:07:27 +0300

    тг

--- TG_BOT/TG_BOT/Program.cs
+++ TG_BOT/TG_BOT/Program.cs
@@ -1,144 +1,141 @@
-﻿using System;
-using System.Resources;
-using System.Threading;
-using System.Threading.Tasks;
-using Telegram.Bot;
+﻿using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
-using Telegram.Bot.Requests;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
-namespace TG_BOT
+var botClient = new TelegramBotClient("5758951049:AAFD6BCcrdlqHSfWUxF7syESPZKvPAojcSU");
+
+using CancellationTokenSource cts = new();
+
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+ReceiverOptions receiverOptions = new()
 {
-    internal class Program
-    {
-        private static async Task Main(string[] args)
-        {
-            var botClient = new TelegramBotClient("5758951049:AAFD6BCcrdlqHSfWUxF7syESPZKvPAojcSU");
+    AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+};
 
-            using CancellationTokenSource cts = new();
-            var kol = "Blender модель 400-1500\r\n" + "JS 400-1500";
-            var sost = "Здравствуй, мученник :)\r\n" + "TG @BronPluzy , @esiw_yar";
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
 
-            ReceiverOptions receiverOptions = new()
-            {
-                AllowedUpdates = Array.Empty<UpdateType>()
-            };
+var me = await botClient.GetMeAsync();
 
-            botClient.StartReceiving(
-                updateHandler: HandleUpdateAsync,
-                pollingErrorHandler: HandlePollingErrorAsync,
-                receiverOptions: receiverOptions,
-                cancellationToken: cts.Token
-            );
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
 
-            var me = await botClient.GetMeAsync();
-            Console.WriteLine($"Start listening for @{me.Username}");
-            Console.ReadLine();
-            cts.Cancel();
+// Send cancellation request to stop bot
+cts.Cancel();
 
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 
+{
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if (update.Message is not { } message)
+        return;
+    // Only process text messages
+    if (message.Text is not { } messageText)
+        return;
 
-
-            async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-            {
-                if (update.Message is not { } message)
-                    return;
-
-                if (message.Text is not { } messageText)
-                    return;
-
-                var chatId = message.Chat.Id;
-                Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
-
-
-                ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
-                {
-                new KeyboardButton[] { "Цены " },
-                new KeyboardButton[] { "Примеры работ" },
-                new KeyboardButton[] {"Скидки"},
-                new KeyboardButton[] {"Контакты"}
+    var chatId = message.Chat.Id;
+    var kol = "Blender модель 400-1500\r\n" + "JS 400-1500";
+    var sost = "Здравствуй, мученник :)\r\n" + "TG @BronPluzy , @esiw_yar";
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+    ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+{
+    new KeyboardButton[] {"Примеры работ JS", "Примеры работ Blender", "Скидки", "Контакты","Цены"},
 })
-                {
-                    ResizeKeyboard = true
-                };
+    {
+        ResizeKeyboard = true
+    };
+
+    Message sentMessage = await botClient.SendTextMessageAsync(
+        chatId: chatId,
+        text: "Делай заказ как можно скорее",
+        replyMarkup: replyKeyboardMarkup,
+        cancellationToken: cancellationToken);
+
+    // Echo received message text
+    Message sentMessage1 = await botClient.SendTextMessageAsync(
+        chatId: chatId,
+        text: "Чем быстрее оформишь заказ,тем быстрее работа будет выполнена",
+        cancellationToken: cancellationToken);
+
+    if (messageText == "Контакты") //4
+    {
+        await botClient.SendTextMessageAsync(chatId, sost, cancellationToken: cancellationToken);
+
+        await botClient.SendPhotoAsync(
+         chatId: chatId,
+        photo: InputFile.FromUri("https://github.com/Esiw12/ZADACHI_ALG/blob/main/COMANDA.jpg?raw=true"),
+        caption: "Friends",
+        parseMode: ParseMode.Html,
+        cancellationToken: cancellationToken);
 
 
+    }
+    if (messageText == "Скидки") //2
+    {
+        await botClient.SendTextMessageAsync(chatId, "Скидок нет,Я Даже с друзей беру по 1к за анимацию.", cancellationToken: cancellationToken);
+    }
+    if (messageText == "Примеры работ Blender") //1
+    {
 
+        await botClient.SendVideoAsync(
+        chatId: chatId,
+            video: InputFile.FromUri("https://github.com/Esiw12/ZADACHI_ALG/raw/main/Tank.mp4"),
 
-                if (messageText == "Контакты" || messageText == "4") //4
-                {
-                    await botClient.SendTextMessageAsync(chatId, sost, cancellationToken: cancellationToken);
-
-                    await botClient.SendPhotoAsync(
-                     chatId: chatId,
-                    photo: InputFile.FromUri("https://github.com/Esiw12/ZADACHI_ALG/blob/main/COMANDA.jpg?raw=true"),
-                    caption: "Friends",
-                    parseMode: ParseMode.Html,
-                    cancellationToken: cancellationToken);
-
-                }
-                if (messageText == "Скидки" || messageText == "2") //2
-                {
-                    await botClient.SendTextMessageAsync(chatId, "Скидок нет,Я Даже с друзей беру по 1к за анимацию.", cancellationToken: cancellationToken);
-                }
-                if (messageText == "Примеры работ Blender" || messageText == "1" || messageText == "Примеры работ") //1
-                {
-
-                    await botClient.SendVideoAsync(
-                    chatId: chatId,
-                        video: InputFile.FromUri("https://github.com/Esiw12/ZADACHI_ALG/blob/main/Tank.mp4"),
-
-                    thumbnail: InputFile.FromUri("https://raw.githubusercontent.com/TelegramBots/book/master/src/2/docs/thumb-clock.jpg"),
-                    supportsStreaming: true,
-                    cancellationToken: cancellationToken);
-                    await botClient.SendMediaGroupAsync(
-                    chatId: chatId,
-                    media: new IAlbumInputMedia[]
-                     {
+        thumbnail: InputFile.FromUri("https://github.com/Esiw12/ZADACHI_ALG/raw/main/Tank.mp4"),
+        supportsStreaming: true,
+        cancellationToken: cancellationToken);
+        await botClient.SendMediaGroupAsync(
+        chatId: chatId,
+        media: new IAlbumInputMedia[]
+         {
                       new InputMediaPhoto(
                         InputFile.FromUri("https://github.com/Esiw12/ZADACHI_ALG/blob/main/photo_2023-09-21_15-53-52.jpg?raw=true")),
                         new InputMediaPhoto(
                          InputFile.FromUri("https://github.com/Esiw12/ZADACHI_ALG/blob/main/photo_2023-10-07_16-03-36.jpg?raw=true")),
-                     },
-                        cancellationToken: cancellationToken); //АЛЬбом
+         },
+            cancellationToken: cancellationToken); //АЛЬбом
 
 
-                }
-                if (messageText == "Примеры работ JS " || messageText == "5" || messageText == "Примеры работ")
-                {
-                    await botClient.SendMediaGroupAsync(
-                    chatId: chatId,
-                media: new IAlbumInputMedia[]
-                {
+    }
+    if (messageText == "Примеры работ JS")
+    {
+        await botClient.SendMediaGroupAsync(
+        chatId: chatId,
+    media: new IAlbumInputMedia[]
+    {
                     new InputMediaPhoto(
                     InputFile.FromUri("https://github.com/Esiw12/ZADACHI_ALG/blob/main/photo_2023-10-11_19-42-01.jpg?raw=true")),
                     new InputMediaPhoto(
                     InputFile.FromUri("https://github.com/Esiw12/ZADACHI_ALG/blob/main/photo_2023-10-11_19-46-48.jpg?raw=true")),
-                },
-                cancellationToken: cancellationToken);
-                }
-                if (messageText == "Цены" || messageText == "3") //3
-                {
-                    await botClient.SendTextMessageAsync(chatId, kol, cancellationToken: cancellationToken);
-
-
-                }
-            }
-
-            static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-            {
-                var ErrorMessage = exception switch
-                {
-                    ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-                    _ => exception.ToString()
-                };
-
-                Console.WriteLine(ErrorMessage);
-                return Task.CompletedTask;
-            }
-        }
+    },
+    cancellationToken: cancellationToken);
     }
+    if (messageText == "Цены") //3
+    {
+        await botClient.SendTextMessageAsync(chatId, kol, cancellationToken: cancellationToken);
+
+
+    }
+
+
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
 }
